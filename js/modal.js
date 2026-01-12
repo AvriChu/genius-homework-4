f25b5d2781838935984c2579029cf269a088866c
(Avi) Anastasia <nastona367@gmail.com>
2026-01-13 00:34:35 +0200

    Made Module 18

--- js/modal.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.open-btn');
-const modalBtnClose = document.querySelector('.cross-button');
-
-const toggleModal = () => modal.classList.toggle('is-hidden');
-
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
