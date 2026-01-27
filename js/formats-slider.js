291cee1d1dd816c54d3bfd00513e976bc5d92797
(Avi) Anastasia <nastona367@gmail.com>
2026-01-27 19:44:18 +0200

    Made edits for slider

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -2,24 +2,38 @@ const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 16,
+
   breakpoints: {
-    768: {
-      slidesPerView: 2,
-    },
-    960: {
-      slidesPerView: 3,
-    },
+    768: { slidesPerView: 2 },
+    960: { slidesPerView: 3 },
   },
+
   pagination: {
     el: '.pagination',
     bulletClass: 'pagination__button',
     bulletActiveClass: 'pagination__button--active',
+
+    renderBullet: function (index, className) {
+      if (index < 3) {
+        return `<span class="${className}" data-index="${index}"></span>`;
+      }
+      return '';
+    },
   },
+
+  on: {
+    slideChange: function () {
+      const realIndex = this.realIndex % 3;
+      const bullets = document.querySelectorAll('.pagination__button');
+
+      bullets.forEach((b, i) => {
+        b.classList.toggle('pagination__button--active', i === realIndex);
+      });
+    },
+  },
+
   navigation: {
     nextEl: '.button-with-arrow.right',
     prevEl: '.button-with-arrow.left',
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
